eba39a6c0900b57ef85337ed680ec80a448575de
Joydip <joydip.mondal@gmail.com>
2022-12-01 12:58:05 +0530

    service and axios completed

--- codes/day-14/product-management-system/src/services/productService.ts
+++ codes/day-14/product-management-system/src/services/productService.ts
@@ -1,15 +1,19 @@
-export const getAllProducts = () => {
+import axiosInstance from "../config/axios.config"
+import { ApiResponse } from "../models/api-response.model"
+import { Product } from "../models/product.model"
 
+export const getAllProducts = () => {
+    return axiosInstance.get<ApiResponse<Product[]>>('')
 }
 export const getProductById = (id: number) => {
-
+    return axiosInstance.get<ApiResponse<Product>>(`/${id}`)
 }
-export const addProduct = () => {
-
+export const addProduct = (product: Product) => {
+    return axiosInstance.post<ApiResponse<Product>>('', product)
 }
-export const updateProduct = () => {
-
+export const updateProduct = (product: Product, id: number) => {
+    return axiosInstance.put<ApiResponse<Product>>(`/${id}`, product)
 }
-export const deleteProduct = () => {
-
+export const deleteProduct = (id: number) => {
+    return axiosInstance.delete<ApiResponse<Product>>(`/${id}`)
 }
